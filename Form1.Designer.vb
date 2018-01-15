4af06024c604e4e31c4a637a1e958bec39e9f2bd
nach1221 <35469309+nach1221@users.noreply.github.com>
2018-01-15 19:28:41 -0300

    Codigo de diseño y funcionamiento (Alpha)

---
+++ Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmLoLClientModifier
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLoLClientModifier))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(121, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 29)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Buscar Archivo"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Black
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(359, 186)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Paso 1: Selecciona tu archivo LeagueClientSettings.yaml"
+        Me.GroupBox1.UseCompatibleTextRendering = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Image = Global.WindowsApplication1.My.Resources.Resources.fondo1
+        Me.Label1.Location = New System.Drawing.Point(17, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(326, 65)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " Podras encontrarlo dentro de la carpeta Config en tu carpeta de" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " League of Le" &
+    "gends. Por defecto se encuentra en Disco Local C:\" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " Riot Games\League of Legend" &
+    "s\Config\LeagueClientSettings.yaml" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.ListBox2)
+        Me.GroupBox2.Controls.Add(Me.PictureBox1)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.GroupBox2.Location = New System.Drawing.Point(382, 28)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(403, 186)
+        Me.GroupBox2.TabIndex = 15
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Paso 2: Selecciona tu idioma y cual deseas instalar."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Location = New System.Drawing.Point(266, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Idioma a Instalar"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Enabled = False
+        Me.ListBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Items.AddRange(New Object() {"Alemán", "Checo", "Español (Latinoamerica)", "Español (España)", "Ingles (Estados Unidos)", "Ingles (Inglaterra)", "Frances", "Griego", "Italiano", "Japonés", "Polaco", "Portugues", "Ruso", "Turco"})
+        Me.ListBox2.Location = New System.Drawing.Point(239, 43)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(144, 121)
+        Me.ListBox2.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.arrow_1_como_objeto_inteligente_1
+        Me.PictureBox1.Location = New System.Drawing.Point(182, 80)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(42, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Location = New System.Drawing.Point(59, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Idioma Actual"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Enabled = False
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Alemán", "Checo", "Español (Latinoamerica)", "Español (España)", "Ingles (Estados Unidos)", "Ingles (Inglaterra)", "Frances", "Griego", "Italiano", "Japonés", "Polaco", "Portugues", "Ruso", "Turco"})
+        Me.ListBox1.Location = New System.Drawing.Point(25, 43)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(144, 121)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(322, 241)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(139, 36)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Confirmar Cambios"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'frmLoLClientModifier
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.fondo
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(804, 311)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmLoLClientModifier"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LoLClientModifier (Alpha para LCK)"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,160 @@
+﻿Public Class frmLoLClientModifier
+    Private idiac, idinu As Boolean
+    Private idiact, idinue, strfilename As String
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        idiac = True
+        If ListBox1.SelectedItem = "Alemán" Then
+            idiact = "de_DE"
+        End If
+        If ListBox1.SelectedItem = "Checo" Then
+            idiact = "cs_CZ"
+        End If
+        If ListBox1.SelectedItem = "Español (Latinoamerica)" Then
+            idiact = "es_MX"
+        End If
+        If ListBox1.SelectedItem = "Español (España)" Then
+            idiact = "es_ES"
+        End If
+        If ListBox1.SelectedItem = "Ingles (Estados Unidos)" Then
+            idiact = "en_US"
+        End If
+        If ListBox1.SelectedItem = "Ingles (Inglaterra)" Then
+            idiact = "en_GB"
+        End If
+        If ListBox1.SelectedItem = "Ingles (Inglaterra)" Then
+            idiact = "en_GB"
+        End If
+        If ListBox1.SelectedItem = "Ingles (Inglaterra)" Then
+            idiact = "en_GB"
+        End If
+        If ListBox1.SelectedItem = "Frances" Then
+            idiact = "fr_FR"
+        End If
+        If ListBox1.SelectedItem = "Griego" Then
+            idiact = "el_GR"
+        End If
+        If ListBox1.SelectedItem = "Italiano" Then
+            idiact = "it_IT"
+        End If
+        If ListBox1.SelectedItem = "Japonés" Then
+            idiact = "ja_JP"
+        End If
+        If ListBox1.SelectedItem = "Polaco" Then
+            idiact = "pl_PL"
+        End If
+        If ListBox1.SelectedItem = "Portugues" Then
+            idiact = "pt_BR"
+        End If
+        If ListBox1.SelectedItem = "Ruso" Then
+            idiact = "ru_RU"
+        End If
+        If ListBox1.SelectedItem = "Turco" Then
+            idiact = "tr_TR"
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If idiac = True And idinu = True Then
+            Button2.Enabled = True
+            Timer1.Enabled = False
+        End If
+    End Sub
+
+    Private i As Single
+    Private selectedfile As String
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim content As String
+
+        ' lee el contenido actual del texto
+        content = System.IO.File.ReadAllText(strfilename)
+
+        If content.Contains(idiact) = False Then
+            MsgBox("El idioma actual especificado no es válido o correcto.", MsgBoxStyle.Critical, "Error")
+            Exit Sub
+        End If
+        If idiact = idinue Then
+            MsgBox("El idioma seleccionado es el mismo que el actual.", MsgBoxStyle.Critical, "Error")
+        End If
+        ' modifica el idioma
+        content = content.Replace(idiact, idinue)
+
+        ' escribe el nuevo contenido
+        System.IO.File.WriteAllText(strfilename, content)
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+        idinu = True
+        If ListBox2.SelectedItem = "Alemán" Then
+            idinue = "de_DE"
+        End If
+        If ListBox2.SelectedItem = "Checo" Then
+            idinue = "cs_CZ"
+        End If
+        If ListBox2.SelectedItem = "Español (Latinoamerica)" Then
+            idinue = "es_MX"
+        End If
+        If ListBox2.SelectedItem = "Español (España)" Then
+            idinue = "es_ES"
+        End If
+        If ListBox2.SelectedItem = "Ingles (Estados Unidos)" Then
+            idinue = "en_US"
+        End If
+        If ListBox2.SelectedItem = "Ingles (Inglaterra)" Then
+            idinue = "en_GB"
+        End If
+        If ListBox2.SelectedItem = "Ingles (Inglaterra)" Then
+            idinue = "en_GB"
+        End If
+        If ListBox2.SelectedItem = "Ingles (Inglaterra)" Then
+            idinue = "en_GB"
+        End If
+        If ListBox2.SelectedItem = "Frances" Then
+            idinue = "fr_FR"
+        End If
+        If ListBox2.SelectedItem = "Griego" Then
+            idinue = "el_GR"
+        End If
+        If ListBox2.SelectedItem = "Italiano" Then
+            idinue = "it_IT"
+        End If
+        If ListBox2.SelectedItem = "Japonés" Then
+            idinue = "ja_JP"
+        End If
+        If ListBox2.SelectedItem = "Polaco" Then
+            idinue = "pl_PL"
+        End If
+        If ListBox2.SelectedItem = "Portugues" Then
+            idinue = "pt_BR"
+        End If
+        If ListBox2.SelectedItem = "Ruso" Then
+            idinue = "ru_RU"
+        End If
+        If ListBox2.SelectedItem = "Turco" Then
+            idinue = "tr_TR"
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+
+        fd.Title = "Selecciona tu LeagueClientSettings.yaml"
+        fd.InitialDirectory = "C:\"
+        fd.Filter = "All files (*.*)|*.*|All files (*.*)|*.*"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            strfilename = fd.FileName
+        End If
+
+        ListBox1.Enabled = True
+        ListBox2.Enabled = True
+    End Sub
+End Class
